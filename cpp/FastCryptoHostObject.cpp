281b7fb70b1c8108ff2fa01fd8fae3177e0b38fb
Tomas Ravinskas <tomas.rav@gmail.com>
2022-07-25 18:41:10 +0300

    Replace runtime errors

--- cpp/FastCryptoHostObject.cpp
+++ cpp/FastCryptoHostObject.cpp
@@ -8,13 +8,18 @@ using namespace facebook;
 // TODO: Create macros for this so we don't have to repeat ourselves for each JSI func?
 
 std::vector<jsi::PropNameID> FastCryptoHostObject::getPropertyNames(jsi::Runtime& runtime) {
-	throw std::runtime_error("Not yet implemented!");
-}
+	vector<PropNameID> result;
+
+	result.push_back(PropNameID::forUtf8(runtime, "test"));
+
+	return result;}
 
 jsi::Value FastCryptoHostObject::get(jsi::Runtime& runtime, const jsi::PropNameID& propNameId) {
-  auto propName = propNameId.utf8(runtime);
+	auto propName = propNameId.utf8(runtime);
 
-	throw std::runtime_error("Not yet implemented!");
-}
+	if (propName == "test") {
+		return Value(runtime, String::createFromUtf8(runtime, "Hello, World!"));
+	}
 
+	return Value::undefined();}
 } // namespace margelo
